c049bdacb1700f7db23f1163a2a453f6ede07029
Draqqonfly <dragovonnova@gmail.com>
2020-04-04 17:56:58 +0200

    Initial commit with spawning circles when clicked

---
+++ Assets/Scripts/ColorsController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorsController : MonoBehaviour
+{
+    public Color topLeftColor;
+    public Color topRightColor;
+    public Color bottomLeftColor;
+    public Color bottomRightColor;
+    public GameObject shape;
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(Touch touch in Input.touches)
+        {
+
+        }
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            Vector2 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            GameObject shapeObj = Instantiate(shape, objPosition, Quaternion.identity);
+            if (objPosition.x < 0 && objPosition.y > 0)
+                shapeObj.GetComponent<SpriteRenderer>().color = topLeftColor;
+            else if (objPosition.x > 0 && objPosition.y > 0)
+                shapeObj.GetComponent<SpriteRenderer>().color = topRightColor;
+            else if (objPosition.x < 0 && objPosition.y < 0)
+                shapeObj.GetComponent<SpriteRenderer>().color = bottomLeftColor;
+            else if (objPosition.x > 0 && objPosition.y < 0)
+                shapeObj.GetComponent<SpriteRenderer>().color = bottomRightColor;
+        }
+    }
+}
---
+++ Assets/Scripts/ShapeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeController : MonoBehaviour
+{
+    bool rising;
+    private float scaleUpFactor = 100.3f;
+    private float scaleDownFactor = 25f;
+
+    void Start()
+    {
+        rising = true;
+        Debug.Log(transform.localScale);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+            rising = false;
+
+        if (rising && Input.GetMouseButton(0))
+            Expand();
+        else
+            Shrink();
+    }
+
+    private void Expand()
+    {
+        Vector2 scale = transform.localScale;
+        scale.x += scaleUpFactor * Time.deltaTime;
+        scale.y += scaleUpFactor * Time.deltaTime;
+        transform.localScale = scale;
+        Debug.Log(transform.localScale);
+    }
+
+    private void Shrink()
+    {
+        Vector2 scale = transform.localScale;
+        scale.x -= scaleDownFactor * Time.deltaTime;
+        scale.y -= scaleDownFactor * Time.deltaTime;
+        transform.localScale = scale;
+        Debug.Log(transform.localScale);
+        if (transform.localScale.x < 0.01f)
+            Destroy(gameObject);
+    }
+}

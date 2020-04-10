5069f635e7e5791d9115aa2e1b86031c1701bbd2
Dragoonov <dragovonnova@gmail.com>
2020-04-10 07:57:43 +0200

    Added part of locking/blocking mechanism

---
+++ Assets/Scripts/BlockController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockController : MonoBehaviour
+{
+    BlueprintController parent;
+    public GameObject unblockShape;
+    string targetName;
+    void Start()
+    {
+        parent = transform.parent.gameObject.GetComponent<BlueprintController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject desiredShape = GameObject.Find(targetName + "(Clone)");
+        if (desiredShape != null && desiredShape.transform.localScale.x > 30)
+        {
+            parent.Blocked = false;
+        }
+    }
+}
---
+++ Assets/Scripts/LockController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockController : MonoBehaviour
+{
+    BlueprintController parent;
+    public GameObject unlockShape;
+    void Start()
+    {
+        parent = transform.parent.gameObject.GetComponent<BlueprintController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject desiredShape = GameObject.Find(unlockShape.name + "(Clone)");
+        if(desiredShape != null && desiredShape.transform.localScale.x > 30)
+        {
+            parent.Locked = false;
+        }
+    }
+}

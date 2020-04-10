928a8d63f2978b73dc010a21605c950a99af0dc2
Dragoonov <dragovonnova@gmail.com>
2020-04-10 07:28:40 +0200

    Added help panel

--- Assets/Scripts/ColorsController.cs
+++ Assets/Scripts/ColorsController.cs
@@ -8,68 +8,73 @@ public class ColorsController : MonoBehaviour
     public BlueprintController topRightBlueprint;
     public BlueprintController bottomLeftBlueprint;
     public BlueprintController bottomRightBlueprint;
+    public GameObject helpModal;
 
     GameObject holderCopy;
+    public bool paused;
 
     // Update is called once per frame
     void Update()
     {
-        foreach(Touch touch in Input.touches)
+        if (!paused)
         {
+            foreach (Touch touch in Input.touches)
+            {
 
-        }
-        if(Input.GetMouseButtonDown(0))
-        {
-            Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-            Vector2 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(objPosition, Vector2.zero);
-            if(hit.collider != null && hit.collider.gameObject.name == "ColorHolder")
-            {
-                holderCopy = Instantiate(hit.collider.gameObject,new Vector3(objPosition.x,objPosition.y,5f),Quaternion.identity);
-                holderCopy.gameObject.transform.localScale = new Vector3(10, 10, 10);
-                ColorHolderController copyController = holderCopy.GetComponent<ColorHolderController>();
-                copyController.isDragging = true;
-                copyController.assignableShapes =
-                    new HashSet<string>(GameObject.Find("ColorHolder").GetComponent<ColorHolderController>().assignableShapes);
-                DisableAssignableBlueprints();
             }
-            else
-            {
-                if (objPosition.x < 0 && objPosition.y > 0 && IsBlueprintActivated(topLeftBlueprint))
-                {
-                    topLeftBlueprint.CreateShape(objPosition);
-                }
-                else if (objPosition.x > 0 && objPosition.y > 0 && IsBlueprintActivated(topRightBlueprint))
-                {
-                    topRightBlueprint.CreateShape(objPosition);
-                }
-                else if (objPosition.x < 0 && objPosition.y < 0 && IsBlueprintActivated(bottomLeftBlueprint))
-                {
-                    bottomLeftBlueprint.CreateShape(objPosition);
-                }
-                else if (objPosition.x > 0 && objPosition.y < 0 && IsBlueprintActivated(bottomRightBlueprint))
-                {
-                    bottomRightBlueprint.CreateShape(objPosition);
-                }
-            }
-        }
-        if(Input.GetMouseButtonUp(0))
-        {
-            if(holderCopy != null)
+            if (Input.GetMouseButtonDown(0))
             {
                 Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
                 Vector2 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
-                Color holderColor = holderCopy.GetComponent<SpriteRenderer>().color;
-                RaycastHit2D hit = Physics2D.Raycast(objPosition, Vector3.back, 10f);
-                Debug.Log("Hit: " + hit.collider.gameObject + ", started on position: " + objPosition);
-                if (hit.collider != null)
+                RaycastHit2D hit = Physics2D.Raycast(objPosition, Vector2.zero);
+                if (hit.collider != null && hit.collider.gameObject.name == "ColorHolder")
                 {
-                    EnableAssignableBlueprints();
-                    GameObject obj = hit.collider.gameObject;
-                    Debug.Log(holderCopy.GetComponent<ColorHolderController>().assignableShapes.Count);
-                    obj.GetComponent<SpriteRenderer>().color = holderColor;
-                    Destroy(holderCopy);
-                    holderCopy = null;
+                    holderCopy = Instantiate(hit.collider.gameObject, new Vector3(objPosition.x, objPosition.y, 5f), Quaternion.identity);
+                    holderCopy.gameObject.transform.localScale = new Vector3(10, 10, 10);
+                    ColorHolderController copyController = holderCopy.GetComponent<ColorHolderController>();
+                    copyController.isDragging = true;
+                    copyController.assignableShapes =
+                        new HashSet<string>(GameObject.Find("ColorHolder").GetComponent<ColorHolderController>().assignableShapes);
+                    DisableAssignableBlueprints();
+                }
+                else if (hit.collider == null || hit.collider.gameObject.name.Contains("Clone"))
+                {
+                    if (objPosition.x < 0 && objPosition.y > 0 && IsBlueprintActivated(topLeftBlueprint))
+                    {
+                        topLeftBlueprint.CreateShape(objPosition);
+                    }
+                    else if (objPosition.x > 0 && objPosition.y > 0 && IsBlueprintActivated(topRightBlueprint))
+                    {
+                        topRightBlueprint.CreateShape(objPosition);
+                    }
+                    else if (objPosition.x < 0 && objPosition.y < 0 && IsBlueprintActivated(bottomLeftBlueprint))
+                    {
+                        bottomLeftBlueprint.CreateShape(objPosition);
+                    }
+                    else if (objPosition.x > 0 && objPosition.y < 0 && IsBlueprintActivated(bottomRightBlueprint))
+                    {
+                        bottomRightBlueprint.CreateShape(objPosition);
+                    }
+                }
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                if (holderCopy != null)
+                {
+                    Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+                    Vector2 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+                    Color holderColor = holderCopy.GetComponent<SpriteRenderer>().color;
+                    RaycastHit2D hit = Physics2D.Raycast(objPosition, Vector3.back, 10f);
+                    Debug.Log("Hit: " + hit.collider.gameObject + ", started on position: " + objPosition);
+                    if (hit.collider != null)
+                    {
+                        EnableAssignableBlueprints();
+                        GameObject obj = hit.collider.gameObject;
+                        Debug.Log(holderCopy.GetComponent<ColorHolderController>().assignableShapes.Count);
+                        obj.GetComponent<SpriteRenderer>().color = holderColor;
+                        Destroy(holderCopy);
+                        holderCopy = null;
+                    }
                 }
             }
         }
@@ -118,5 +123,28 @@ public class ColorsController : MonoBehaviour
     private bool IsBlueprintActivated(BlueprintController controller)
     {
         return controller != null && controller.isActiveAndEnabled;
+    }
+
+    public void Open()
+    {
+        helpModal.SetActive(true);
+        helpModal.GetComponent<HelpController>().Initialize();
+        paused = true;
+    }
+
+    public void Close()
+    {
+        helpModal.SetActive(false);
+        paused = false;
+    }
+
+    public void Pause()
+    {
+        paused = true;
+    }
+
+    public void Unpause()
+    {
+        paused = false;
     }
 }
---
+++ Assets/Scripts/HelpController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HelpController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void Initialize()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            GameObject helper = transform.Find("Helper" + i).gameObject;
+            GameObject tint1 = helper.transform.GetChild(0).gameObject;
+            GameObject tint2 = helper.transform.GetChild(1).gameObject;
+            GameObject result = helper.transform.GetChild(2).gameObject;
+            Color color1 = FindColorForTint(tint1);
+            Color color2 = FindColorForTint(tint2);
+            tint1.GetComponent<Image>().color = color1;
+            tint2.GetComponent<Image>().color = color2;
+            result.GetComponent<Image>().color = MixColors(color1, color2);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private Color FindColorForTint(GameObject tint)
+    {
+        string tintName = tint.name.Substring(0, tint.name.Length - 4);
+        return GameObject.Find(tintName + "Blueprint").GetComponent<SpriteRenderer>().color;
+    }
+
+    private Color MixColors(Color color1, Color color2)
+    {
+        return (color1 + color2) / 2;
+    }
+}
